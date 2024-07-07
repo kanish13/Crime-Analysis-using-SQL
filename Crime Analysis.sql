1658064579362528baf56a97e52b5254d94794a3
Kanish <111358462+kanish13@users.noreply.github.com>
2024-07-07 13:09:10 +0530

    Update Crime Analysis.sql

--- Crime Analysis.sql
+++ Crime Analysis.sql
@@ -1,166 +1,159 @@
-﻿
 -- DATABASE CREATION
 
-create database CrimeAnalysis
+CREATE DATABASE CrimeAnalysis
 
 -- DATA RETRIEVAL
 
--- a) homicide
+-- a) HOMICIDE
 
-SELECT year,state_name,homicide FROM CrimeAnalysis..crimes
+SELECT year, state_name, homicide FROM CrimeAnalysis..crimes
 
--- b) rape
+-- b) RAPE
 
-SELECT year,state_name,rape_legacy FROM CrimeAnalysis..crimes
+SELECT year, state_name, rape_legacy FROM CrimeAnalysis..crimes
 
--- c) robbery
+-- c) ROBBERY
 
-SELECT year,state_name,robbery FROM CrimeAnalysis..crimes
+SELECT year, state_name, robbery FROM CrimeAnalysis..crimes
 
 -- FILTERING DATA
 
---a) crimes occurred in 2003
+--a) CRIMES OCCURRED IN 2003
 
 SELECT * FROM CrimeAnalysis..crimes 
-WHERE year=2003
+WHERE year = 2003
 
---b) homicide occurred in 2003
+--b) HOMICIDE OCCURRED IN 2003
 
-SELECT year,state_name, homicide FROM CrimeAnalysis..crimes 
-WHERE year=2003
+SELECT year, state_name, homicide FROM CrimeAnalysis..crimes 
+WHERE year = 2003
 
---c) rapes occurred in New York
+--c) RAPES OCCURRED IN NEW YORK
 
-SELECT year,state_name, rape_legacy FROM CrimeAnalysis..crimes 
-WHERE state_name='New York'
+SELECT year, state_name, rape_legacy FROM CrimeAnalysis..crimes 
+WHERE state_name = 'New York'
 
---d) robbery occurred in ohio
+--d) ROBBERY OCCURRED IN OHIO
 
-SELECT year,state_name, robbery FROM CrimeAnalysis..crimes 
-WHERE state_abbr='OH'
+SELECT year, state_name, robbery FROM CrimeAnalysis..crimes 
+WHERE state_abbr = 'OH'
 
 -- AGGREGATION AND GROUPING
 
---a) total number of homicide,rape,roberry in 2003
+--a) TOTAL NUMBER OF HOMICIDE, RAPE, ROBBERY IN 2003
 
-SELECT (SUM(homicide)+SUM(rape_legacy)+sum(robbery)) AS TOTAL_CRIME FROM CrimeAnalysis..crimes
-WHERE year=2003
+SELECT (SUM(homicide) + SUM(rape_legacy) + SUM(robbery)) AS TOTAL_CRIME FROM CrimeAnalysis..crimes
+WHERE year = 2003
 
---b) average number of murder per year
+--b) AVERAGE NUMBER OF MURDER PER YEAR
 
-SELECT ROUND(SUM(homicide)/COUNT(homicide),2) AS AVERAGE_MURDER FROM CrimeAnalysis..crimes
-WHERE year=2003
+SELECT ROUND(SUM(homicide) / COUNT(homicide), 2) AS AVERAGE_MURDER FROM CrimeAnalysis..crimes
+WHERE year = 2003
 
 -- ADVANCED FILTERING
 
---a) retrieve homicide records where the number of offenses is greater than a 1000
+--a) RETRIEVE HOMICIDE RECORDS WHERE THE NUMBER OF OFFENSES IS GREATER THAN 1000
 
-SELECT year, state_name,homicide FROM CrimeAnalysis..crimes
-WHERE homicide>1000
+SELECT year, state_name, homicide FROM CrimeAnalysis..crimes
+WHERE homicide > 1000
 
---b) retrieve property_crime records where the number of fatalities is above a 30,000
+--b) RETRIEVE PROPERTY_CRIME RECORDS WHERE THE NUMBER OF FATALITIES IS ABOVE 30,000
 
-SELECT year, state_name,property_crime FROM CrimeAnalysis..crimes
-WHERE property_crime>1000
+SELECT year, state_name, property_crime FROM CrimeAnalysis..crimes
+WHERE property_crime > 1000
 
 -- DATA CLEANING
 
--- creating temporary table for cleaning
+-- CREATING TEMPORARY TABLE FOR CLEANING
 
 SELECT TOP 0 * INTO crimes_duplicate FROM CrimeAnalysis..crimes;
 
 INSERT INTO crimes_duplicate
 SELECT * FROM CrimeAnalysis..crimes
 
-select * from crimes_duplicate
+SELECT * FROM crimes_duplicate
 
--- deleting the rows where states are empty
+-- DELETING THE ROWS WHERE STATES ARE EMPTY
 
-delete from crimes_duplicate
-where state_abbr is null and state_name is null
+DELETE FROM crimes_duplicate
+WHERE state_abbr IS NULL AND state_name IS NULL
 
 -- STATISTISTICAL ANALYSIS
 
---a) Find the year with the highest number of homicide
+--a) FIND THE YEAR WITH THE HIGHEST NUMBER OF HOMICIDE
 
-select top 1 year,sum(homicide) as homicide_count from CrimeAnalysis.. crimes
-group by year 
-order by homicide_count desc
+SELECT TOP 1 year, SUM(homicide) AS homicide_count FROM CrimeAnalysis..crimes
+GROUP BY year 
+ORDER BY homicide_count DESC
 
---b) Find the city with the highest number of rape
+--b) FIND THE CITY WITH THE HIGHEST NUMBER OF RAPE
 
-select top 1 state_name,rape_legacy from CrimeAnalysis..crimes_duplicate
-order by rape_legacy desc
+SELECT TOP 1 state_name, rape_legacy FROM CrimeAnalysis..crimes_duplicate
+ORDER BY rape_legacy DESC
 
 -- DATA UPDATE
 
--- a) update 1981 ohio motor_vehicle_theft to 9463
+-- a) UPDATE 1981 OHIO MOTOR_VEHICLE_THEFT TO 9463
 
-update CrimeAnalysis..crimes
-set motor_vehicle_theft=9463
-where state_name='Ohio' and year=1981
+UPDATE CrimeAnalysis..crimes
+SET motor_vehicle_theft = 9463
+WHERE state_name = 'Ohio' AND year = 1981
 
-select * from CrimeAnalysis..crimes_duplicate
+SELECT * FROM CrimeAnalysis..crimes_duplicate
 
 -- DATA DELETION
 
--- b) delete crime record in texas in 2003
+-- b) DELETE CRIME RECORD IN TEXAS IN 2003
 
-delete from CrimeAnalysis..crimes_duplicate
-where year=2003 and state_name='Texas'
+DELETE FROM CrimeAnalysis..crimes_duplicate
+WHERE year = 2003 AND state_name = 'Texas'
 
 -- COMPLEX QUERIES
 
---a) Retrieve state_names data where the number of robbery is above average for the 2005
+--a) RETRIEVE STATE_NAMES DATA WHERE THE NUMBER OF ROBBERY IS ABOVE AVERAGE FOR 2005
 
--- step 1: calculate the average 
+-- STEP 1: CALCULATE THE AVERAGE 
 
-select round(sum(robbery)/count(robbery),2) as avg_robbery from CrimeAnalysis..crimes
-where year=2005 
+SELECT ROUND(SUM(robbery) / COUNT(robbery), 2) AS avg_robbery FROM CrimeAnalysis..crimes
+WHERE year = 2005 
 
--- step 2: number of robbery is above average
+-- STEP 2: NUMBER OF ROBBERY IS ABOVE AVERAGE
 
-select state_name from CrimeAnalysis..crimes
-where robbery>(select round(sum(robbery)/count(robbery),2) as avg_robbery from CrimeAnalysis..crimes
-where year=2005) and year=2005 and state_name is not null
+SELECT state_name FROM CrimeAnalysis..crimes
+WHERE robbery > (SELECT ROUND(SUM(robbery) / COUNT(robbery), 2) AS avg_robbery FROM CrimeAnalysis..crimes
+WHERE year = 2005) AND year = 2005 AND state_name IS NOT NULL
 
---b) Calculate the year-over-year growth rate of burglary (2013)
+--b) CALCULATE THE YEAR-OVER-YEAR GROWTH RATE OF BURGLARY (2013)
 
-/* formula for y-o-y
+-- Formula for YoY Growth Rate:
+-- YoY Growth Rate = ((Accidents in Current Year − Accidents in Previous Year) / Accidents in Previous Year) × 100
+
+-- ACCIDENTS IN CURRENT YEAR
+
+SELECT SUM(burglary) AS Current_Year FROM CrimeAnalysis..crimes
+WHERE year = 2013
+
+-- ACCIDENTS IN PREVIOUS YEAR
+
+SELECT SUM(burglary) AS Previous_year FROM CrimeAnalysis..crimes
+WHERE year = 2012
+
+-- YoY CALCULATION
  
-YoY Growth Rate=( 
-(Accidents in Current Year−Accidents in Previous Year)/Accidents in Previous Year
-​)×100
+WITH yearlyBurglary(year, burglary) AS 
+(SELECT year, SUM(burglary) FROM CrimeAnalysis..crimes
+WHERE year IN (2012, 2013)
+GROUP BY year)
 
-*/
-
---Accidents in Current Year
-
-select sum(burglary) as Current_Year from CrimeAnalysis..crimes
-where year=2013
-
---Accidents in Previous Year
-
-select sum(burglary) as Previous_year from CrimeAnalysis..crimes
-where year=2012
-
---YoY Calculation
- 
-with yearlyBurglary(year,burglary) as 
-(select year,sum(burglary) from CrimeAnalysis..crimes
-where year in (2012,2013)
-group by year)
-
-select 
-round(((y2013.burglary-y2012.burglary)/y2012.burglary)*100,2)
-from yearlyBurglary y2013
-left join yearlyBurglary y2012
-on 
-y2013.year=y2012.year+1
-where y2013.year =2013
+SELECT 
+ROUND(((y2013.burglary - y2012.burglary) / y2012.burglary) * 100, 2)
+FROM yearlyBurglary y2013
+LEFT JOIN yearlyBurglary y2012
+ON y2013.year = y2012.year + 1
+WHERE y2013.year = 2013
 
 -- INDEXING
 
-select burglary from CrimeAnalysis..crimes
+SELECT burglary FROM CrimeAnalysis..crimes
 
-create index ind_burglary on CrimeAnalysis..crimes (burglary)
+CREATE INDEX ind_burglary ON CrimeAnalysis..crimes (burglary)
